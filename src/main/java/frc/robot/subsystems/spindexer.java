5efe58a72dfe60311545b77d0dd4bc7c3df3237f
aarush-mehta-ai <aarush.mehta@student.tdsb.on.ca>
2026-02-13 14:15:40 -0500

    Update spindexer.java

--- src/main/java/frc/robot/subsystems/spindexer.java
+++ src/main/java/frc/robot/subsystems/spindexer.java
@@ -20,18 +20,19 @@ public class spindexer extends SubsystemBase {
 
     private SparkMax mSparkMax = new SparkMax(0, MotorType.kBrushless); 
     private AbsoluteEncoder mEncoder; 
-
+   
     public spindexer() {
        spindexerConfig();   
+        
     }
 
     public void spindexerConfig() {
-         SparkMaxConfig mConfig = new SparkMaxConfig(); 
+    SparkMaxConfig mConfig = new SparkMaxConfig(); 
     //just set it to 0, will probably tune later 
     SimpleMotorFeedforward mFeedforward = new SimpleMotorFeedforward(0, 0,0); 
      //will change in future
-      mConfig.smartCurrentLimit(30);
-      mSparkMax.configure(mConfig, ResetMode.kResetSafeParameters, PersistMode.kNoPersistParameters); 
+    mConfig.smartCurrentLimit(30);
+    mSparkMax.configure(mConfig, ResetMode.kResetSafeParameters, PersistMode.kNoPersistParameters); 
     
     }
 
