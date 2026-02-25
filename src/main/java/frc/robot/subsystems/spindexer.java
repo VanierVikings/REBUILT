fb29173b855abeecb594893887e8a4e882c09f1d
aarush-mehta-ai <aarush.mehta@student.tdsb.on.ca>
2026-02-25 16:30:10 -0500

    Started DutyCycleEncoder or some shit

--- src/main/java/frc/robot/subsystems/spindexer.java
+++ src/main/java/frc/robot/subsystems/spindexer.java
@@ -3,6 +3,7 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.math.controller.SimpleMotorFeedforward;
+import edu.wpi.first.wpilibj.DutyCycleEncoder;
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.RobotBase;
 import edu.wpi.first.wpilibj.TimedRobot;
@@ -19,29 +20,38 @@ import com.revrobotics.AbsoluteEncoder;
 import com.revrobotics.spark.config.SparkMaxConfig;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 
-
-
 public class spindexer extends SubsystemBase {
 
     private SparkMax spindexerMotor;
     private SparkMaxConfig spindexerConfig;
     SparkClosedLoopController spindexController;
+    private DutyCycleEncoder spindexerEncoder;
+
    
     public spindexer() {
         spindexerMotor = new SparkMax(SpindexerConstants.SPINDEXER_MOTOR_ID,MotorType.kBrushless);
-
+        
         spindexerConfig = new SparkMaxConfig(); 
-        spindexerConfig
+        spindexerEncoder = spindexerMotor.getEncoder(); 
+        spindexerConfig.spindexerEncoder
+             .positionConversionFactor(1)
+             .velocityConversionFactor(1); 
+            spindexerConfig
         .smartCurrentLimit(SpindexerConstants.SPINDEXER_CURRENT_LIMIT)
         .idleMode(IdleMode.kCoast);
 
+
         spindexerMotor.configure(spindexerConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+        spindexerEncoder.isConnected(); 
     }
 
 
     public Command spin(double direction){
+            spindexerEncoder.get(); 
             return this.runEnd(() -> spindexerMotor.set(direction), () -> spindexerMotor.set(0));
+            spindexerEncoder.reset(); 
         }
 
 
 }
+
